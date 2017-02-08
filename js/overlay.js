65bd38ec53e15e75498300a99835d62035a9925f
isaacrlee <isaacrlee@gmail.com>
2017-02-08 13:48:49 -0600

    reorg files, change class names

---
+++ js/overlay.js
@@ -0,0 +1,13 @@
+/* Open */
+  function openNav(e) {
+    document.getElementById("severity").style.height = "100%";
+    document.getElementById("symptom").innerHTML = e;
+  }
+  /* Close */
+  function closeNav() {
+    document.getElementById("severity").style.height = "0%";
+    document.getElementById("add").className = "add hidden";
+  }
+  function toggleButton(){
+    document.getElementById("add").className = "add show";
+  }
