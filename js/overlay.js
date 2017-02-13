83de33ff212f3033916a6ba367c2e7c45758f9ae
leelouisa120 <lee.louisa120@gmail.com>
2017-02-13 17:47:05 -0600

    Tried to add functionality to search bar

--- js/overlay.js
+++ js/overlay.js
@@ -11,3 +11,17 @@
   function toggleButton(){
     document.getElementById("add").className = "add show";
   }
+
+// var searchbar = document.getElementById("searchbar");
+// searchbar.addEventListener("keyup", function(event){
+//     if (event.keyCode == 13) {
+//         event.preventDefault();
+//
+//         if (searchbar.value.length != 0) {
+//             console.log(searchbar.value);
+//             openNav(searchbar)
+//             // Run my specific process with my_field.value
+//             my_field.value = '';
+//         }
+//     }
+// }, false);
