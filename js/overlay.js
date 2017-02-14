d5f49893441773d0075a4d43d82947123e521f1a
remyafong <remy.afong@gmail.com>
2017-02-14 00:41:46 -0600

    Add files via upload

--- js/overlay.js
+++ js/overlay.js
@@ -6,14 +6,18 @@
   /* Close */
   function closeNav() {
     document.getElementById("severity").style.height = "0%";
+  }
+
+  function toggleButton(){
+    document.getElementById("add").className = "add show";
+  }
+
+  function closeNavAdded() {
+    document.getElementById("severity").style.height = "0%";
     document.getElementById("add").className = "add hide";
     $(document).ready(function(){
             $('#feedback').fadeIn('slow', function(){
                $('#feedback').delay(1500).fadeOut();
             });
         });
-  }
-
-  function toggleButton(){
-    document.getElementById("add").className = "add show";
   }
